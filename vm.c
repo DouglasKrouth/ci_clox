4659e8222cb1972a1d9229513689f2afd610314e
DouglasKrouth <douglaskrouth@gmail.com>
2023-09-23 20:43:20 -0500

    vm 15.2

--- vm.c
+++ vm.c
@@ -1,5 +1,7 @@
 #include "common.h"
 #include "vm.h"
+#include "debug.h"
+#include <stdio.h>
 
 VM vm;
 
@@ -7,6 +9,31 @@ void initVM() {
 }
 
 void freeVM() {
+}
+
+static InterpretResult run(Chunk* chunk) {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT (vm.chunk->constants.values[READ_BYTE()])
+    for (;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+        disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
+        uint8_t instruction;
+        switch (instruction = READ_BYTE()) {
+            case OP_CONSTANT: {
+                                  Value constant = READ_CONSTANT();
+                                  printValue(constant);
+                                  printf("\n");
+                                  break;
+                              }
+            case OP_RETURN: {
+                                return INTERPRET_OK;
+                            }
+        }
+    }
+
+#undef READ_CONSTANT
+#undef READ_BYTE
 }
 
 InterpretResult interpret(Chunk* chunk) {
